94a2116642b011474c2c4adc642b2a5acee7e5ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-27 17:26:49 +0000

    dotnet@9 9.0.116

--- Formula/d/dotnet@9.rb
+++ Formula/d/dotnet@9.rb
@@ -2,8 +2,8 @@ class DotnetAT9 < Formula
   desc ".NET Core"
   homepage "https://dotnet.microsoft.com/"
   # Source-build tag announced at https://github.com/dotnet/source-build/discussions
-  url "https://github.com/dotnet/dotnet/archive/refs/tags/v9.0.115.tar.gz"
-  sha256 "4e9bb987d5311395e0d516b2db98bcfe02a7eb8054bb8603b6db0b8ea39b81ca"
+  url "https://github.com/dotnet/dotnet/archive/refs/tags/v9.0.116.tar.gz"
+  sha256 "ab2adc9e1ff6639d59c8e8f6afeb1d1e2010da1edc127826e8acd18d8bc2a898"
   license "MIT"
   compatibility_version 1
 
@@ -60,8 +60,8 @@ class DotnetAT9 < Formula
   end
 
   resource "release.json" do
-    url "https://github.com/dotnet/dotnet/releases/download/v9.0.115/release.json"
-    sha256 "72b1fa8d56ded253f234741b88985dab54bac7e079b044ce78292405b83d4fd8"
+    url "https://github.com/dotnet/dotnet/releases/download/v9.0.116/release.json"
+    sha256 "05cea9ff168968ea90b9fdeb04938e333322273cfa5ef0af460fadc6d9280991"
 
     livecheck do
       formula :parent
